31969dc9ee57033bb86cf55f2bf0d6d0a4937f89
CODE <108527636+LudhianeThewriter@users.noreply.github.com>
2025-06-29 09:56:00 +0000

    Add delete and block cloud functions (firebase)

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,38 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+// Delete User by UID
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+exports.deleteUserById = functions.https.onCall(async (data, context) => {
+  // only allow users with admin claim
+  if (!context.auth?.token.admin) {
+    throw new functions.https.HttpsError("permission-denied", "Admins Only");
+  }
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+  const uid = data.uid;
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+  try {
+    await admin.auth().deleteUser(uid);
+    return { message: `User ${uid} deleted successfully` };
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.message);
+  }
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Block Users by disabling their account (Admin only)
+
+export.blockUserById = functions.https.onCall(async ( data, context) => {
+    if(!context.auth?.token.admin){
+        throw new functions.https.HttpsError('permission-denied','Admin only')
+    }
+
+    const uid = data.uid;
+
+
+    try{
+        await admin.auth().updateUser(uid, {disabled:true})
+        return {message : ` User ${uid} has been blocked`}
+    }catch(error){
+        throw new functions.https.HttpsError('internal',error.message)
+    }
+})
