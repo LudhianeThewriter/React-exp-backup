1f6e87d404ae08bd492ca84096a8a3db8e96c6a5
CODE <108527636+LudhianeThewriter@users.noreply.github.com>
2025-07-13 08:43:52 +0000

    Setting Stable version of firebase

--- functions/index.js
+++ functions/index.js
@@ -2,61 +2,74 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+const runtime = {
+  timeoutSeconds: 60,
+  memory: "256MB",
+};
 // Setting role of User
-exports.autoGrantBaseAdmin = functions.auth.user().onCreate(async (user) => {
-  const BASE_Mail = "sharmakaran7910929@gmail.com";
-  if (user.email && user.email.toLowerCase() == BASE_Mail.toLowerCase()) {
-    await admin.auth().setCustomUserClaims(user.uid, { admin: true });
-    console.log(`Admin Claim set for base email : ${user.email}`);
-  }
-});
+exports.autoGrantBaseAdmin = functions
+  .runWith(runtime)
+  .auth.user()
+  .onCreate(async (user) => {
+    const BASE_Mail = "sharmakaran7910929@gmail.com";
+    if (user.email && user.email.toLowerCase() == BASE_Mail.toLowerCase()) {
+      await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+      console.log(`Admin Claim set for base email : ${user.email}`);
+    }
+  });
 
 // List of all users
-exports.listUsers = functions.https.onCall(async (data, context) => {
-  try {
-    const listUsersResult = await admin.auth().listUsers(1000);
-    return listUsersResult.users.map((user) => ({
-      uid: user.uid,
-      email: user.email,
-      disabled: user.disabled,
-      metadata: user.metadata,
-    }));
-  } catch (error) {
-    throw new functions.https.HttpsError("internal", error.message);
-  }
-});
+exports.listUsers = functions
+  .runWith(runtime)
+  .https.onCall(async (data, context) => {
+    try {
+      const listUsersResult = await admin.auth().listUsers(1000);
+      return listUsersResult.users.map((user) => ({
+        uid: user.uid,
+        email: user.email,
+        disabled: user.disabled,
+        metadata: user.metadata,
+      }));
+    } catch (error) {
+      throw new functions.https.HttpsError("internal", error.message);
+    }
+  });
 
 // Delete User by UID
 
-exports.deleteUserById = functions.https.onCall(async (data, context) => {
-  // only allow users with admin claim
-  if (!context.auth?.token.admin) {
-    throw new functions.https.HttpsError("permission-denied", "Admins Only");
-  }
+exports.deleteUserByIdV1 = functions
+  .runWith(runtime)
+  .https.onCall(async (data, context) => {
+    // only allow users with admin claim
+    if (!context.auth?.token.admin) {
+      throw new functions.https.HttpsError("permission-denied", "Admins Only");
+    }
 
-  const uid = data.uid;
+    const uid = data.uid;
 
-  try {
-    await admin.auth().deleteUser(uid);
-    return { message: `User ${uid} deleted successfully` };
-  } catch (error) {
-    throw new functions.https.HttpsError("internal", error.message);
-  }
-});
+    try {
+      await admin.auth().deleteUser(uid);
+      return { message: `User ${uid} deleted successfully` };
+    } catch (error) {
+      throw new functions.https.HttpsError("internal", error.message);
+    }
+  });
 
 // Block Users by disabling their account (Admin only)
 
-exports.blockUserById = functions.https.onCall(async (data, context) => {
-  if (!context.auth?.token.admin) {
-    throw new functions.https.HttpsError("permission-denied", "Admin only");
-  }
+exports.blockUserByIdV1 = functions
+  .runWith(runtime)
+  .https.onCall(async (data, context) => {
+    if (!context.auth?.token.admin) {
+      throw new functions.https.HttpsError("permission-denied", "Admin only");
+    }
 
-  const uid = data.uid;
+    const uid = data.uid;
 
-  try {
-    await admin.auth().updateUser(uid, { disabled: true });
-    return { message: ` User ${uid} has been blocked` };
-  } catch (error) {
-    throw new functions.https.HttpsError("internal", error.message);
-  }
-});
+    try {
+      await admin.auth().updateUser(uid, { disabled: true });
+      return { message: ` User ${uid} has been blocked` };
+    } catch (error) {
+      throw new functions.https.HttpsError("internal", error.message);
+    }
+  });
